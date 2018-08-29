1d3bde5f6e19e7c32de499db0362fd26466179df
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-08-28 20:18:23 -0700

    added posts and comment ajax;

---
+++ views/assets/js/posts-comments.js
@@ -0,0 +1,23 @@
+//posts ajax
+$.ajax({
+   url: '/posts',
+   method: 'GET'
+}).then(function(data) {
+   var ob = data[data.length-1];
+   var p = $('<p>');
+    p.html(ob.category + '<br>' + ob.title + '<br>' + ob.post);
+    $('body').prepend(p);
+});
+
+//comments ajax
+$.ajax({
+   url: '/comments',
+   method: 'GET'
+}).then(function(data) {
+   var commP;
+   for (var i in data) {
+       commP = $('<p>');
+       commP.html(data[i].comment);
+   $('body').append(commP);
+    }
+});
